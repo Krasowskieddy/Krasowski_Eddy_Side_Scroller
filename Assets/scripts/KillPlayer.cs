e9646c76aebcdcffdac061beeb3a9ebfca8995b1
Krasowskieddy <e.krasowski@bdx.ecolescreatives.com>
2026-04-24 16:06:10 +0200

    respawn point

--- Assets/scripts/KillPlayer.cs
+++ Assets/scripts/KillPlayer.cs
@@ -9,7 +9,7 @@ public class KillPlayer : MonoBehaviour
     {
         if (other.gameObject.CompareTag ("Player"))
         {
-            
+            player.transform.position = respawnPoint.position;
         }
     }
 }
